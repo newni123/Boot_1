f7a28a2a6b0eb1fa05bf28ecb75392de653d1457
newni123 <55523379+newni123@users.noreply.github.com>
2019-12-27 17:01:26 +0900

    Initial commit

---
+++ src/main/java/com/iu/b1/member/MemberService.java
@@ -0,0 +1,14 @@
+package com.iu.b1.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	public int memberJoin(MemberVO memberVO) throws Exception{
+		return memberMapper.memberJoin(memberVO);
+	}
+}
---
+++ src/main/java/com/iu/b1/robot/LeftArm.java
@@ -0,0 +1,7 @@
+package com.iu.b1.robot;
+
+import org.springframework.stereotype.Component;
+
+public class LeftArm extends Arm{
+	
+}
